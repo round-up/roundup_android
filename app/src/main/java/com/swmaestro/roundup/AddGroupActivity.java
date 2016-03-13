1b3cbe593599104e667b078471f4560c5206375c
JeongMinCha <cjm9236@naver.com>
2016-03-14 03:53:47 +0900

    created AddGroupActivity and made it show up
    I added two files
    - AddGroupActivityjava file
    - activity_add_group.xml file
    I also added an intent to move onto this activity.
    You can move onto it from now on.

---
+++ app/src/main/java/com/swmaestro/roundup/AddGroupActivity.java
@@ -0,0 +1,16 @@
+package com.swmaestro.roundup;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by JeongMinCha on 16. 3. 14..
+ */
+public class AddGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_group);
+    }
+}
