34f2143919b75ffa6d6325f41a39f6e702cd9c16
JeongMinCha <cjm9236@naver.com>
2016-03-14 16:36:05 +0900

    In some details to make a group, Right button added
    When users specifies details which need some image file to upload, such as logo
    and cover image, they need to tap the button to upload the image files.
    This commit provides image buttons to upload files.
    
    However, this commit only updates XML files, these buttons will not work
    properly.

--- app/src/main/java/com/swmaestro/roundup/AddGroupActivity.java
+++ app/src/main/java/com/swmaestro/roundup/AddGroupActivity.java
@@ -2,6 +2,7 @@ package com.swmaestro.roundup;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 /**
  * Created by JeongMinCha on 16. 3. 14..
