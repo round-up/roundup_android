ede4cec85919a0037c62c86d07769a9323079898
kyeongwan <lk@lemonlab.co.kr>
2016-04-10 16:18:36 +0900

    set club base view

--- app/src/main/java/com/swmaestro/roundup/ClubActivity.java
+++ app/src/main/java/com/swmaestro/roundup/ClubActivity.java
@@ -6,8 +6,14 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by lk on 16. 3. 8..
@@ -15,12 +21,24 @@ import android.view.MenuItem;
 public class ClubActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
 
+    private RecyclerViewAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_club_home);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_menu_send, null));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //onBackPressed()
+            }
+        });
+
         setSupportActionBar(toolbar);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -32,6 +50,17 @@ public class ClubActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        adapter = new RecyclerViewAdapter(getApplicationContext());
+        recyclerView.setAdapter(adapter);
+
+        loadData();
     }
 
     @Override
@@ -40,9 +69,21 @@ public class ClubActivity extends AppCompatActivity implements NavigationView.On
         int id = item.getItemId();
 
 
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    private void loadData(){
+        Feed feed = new Feed("제목", "작성자", R.mipmap.ic_launcher, "내용내용내용내용");
+        List<Feed> feedList = new ArrayList<>();
+
+        for (int i = 0 ; i < 20 ; i++) {
+            feed = new Feed("제목 " + i, "작성자", R.mipmap.ic_launcher, "내용내용내용내용");
+            feedList.add(feed);
+        }
+
+        adapter.setFeedList(feedList);
+    }
+
 }
---
+++ app/src/main/java/com/swmaestro/roundup/Feed.java
@@ -0,0 +1,33 @@
+
+package com.swmaestro.roundup;
+
+public class Feed {
+
+    private String feedTitle;
+    private int feedThumbnail;
+    private String feedContent;
+    private String feedAuthor;
+
+    public Feed(String feedTitle, String feedAuthor, int feedThumbnail, String feedContent) {
+        this.feedTitle = feedTitle;
+        this.feedThumbnail = feedThumbnail;
+        this.feedContent = feedContent;
+        this.feedAuthor = feedAuthor;
+    }
+
+    public String getFeedTitle() {
+        return feedTitle;
+    }
+
+    public String getFeedAuthor(){ return feedAuthor; }
+
+    public int getFeedFileThumbnail() {
+        return feedThumbnail;
+    }
+
+    public String getFeedContent() {
+        return feedContent;
+    }
+
+
+}
