633b5add4e31f62c86e9bdc8027cf60e4e5c3442
JeongMinCha <cjm9236@naver.com>
2016-05-06 19:48:59 +0900

    ClubSummaryFeed class and XML file is made

---
+++ app/src/main/java/com/swmaestro/roundup/com/swmaestro/roundup/home/ClubSummaryFeed.java
@@ -0,0 +1,17 @@
+package com.swmaestro.roundup.com.swmaestro.roundup.home;
+
+import java.util.ArrayList;
+
+/**
+ * Created by JeongMinCha on 16. 5. 6..
+ */
+public class ClubSummaryFeed {
+
+    public String clubName;
+    public String remainingDays;
+    public ArrayList<String> sessionSummary;
+
+    public ClubSummaryFeed() {
+        sessionSummary = new ArrayList<String>();
+    }
+}
