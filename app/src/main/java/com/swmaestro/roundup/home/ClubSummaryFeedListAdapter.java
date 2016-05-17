706cfd29d7d0d3484571b0a140ac0a2c82496ec7
차정민 <JeongMinCha@chajeongmin-ui-MacBook.local>
2016-05-18 02:41:59 +0900

    Fix wrong package organization

--- app/src/main/java/com/swmaestro/roundup/home/ClubSummaryFeedListAdapter.java
+++ app/src/main/java/com/swmaestro/roundup/home/ClubSummaryFeedListAdapter.java
@@ -1,11 +1,10 @@
-package com.swmaestro.roundup.com.swmaestro.roundup.home;
+package com.swmaestro.roundup.home;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
