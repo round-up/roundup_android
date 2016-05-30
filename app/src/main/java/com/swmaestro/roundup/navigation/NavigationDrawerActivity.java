abec6ed9192cc485e2b86547496efdc5976985d5
JeongMinCha <cjm9236@naver.com>
2016-05-30 22:33:00 +0900

    Refactoring methods
    Split long codes in method 'makeNavigationDrawer()' into shorter
    methods. This commit will help you read the code in easier way.

--- app/src/main/java/com/swmaestro/roundup/navigation/NavigationDrawerActivity.java
+++ app/src/main/java/com/swmaestro/roundup/navigation/NavigationDrawerActivity.java
@@ -38,6 +38,11 @@ public class NavigationDrawerActivity extends AppCompatActivity
     private Menu mNavigationMenu;
 
     protected void makeNavigationDrawer() {
+        makeDrawer();
+        makeNavigationView();
+    }
+
+    private void makeDrawer() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -46,7 +51,9 @@ public class NavigationDrawerActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
+    }
 
+    private void makeNavigationView() {
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
