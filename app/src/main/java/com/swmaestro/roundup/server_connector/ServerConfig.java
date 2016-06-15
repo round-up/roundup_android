e07b99cf3bf0a05b61c820bf9584edbf7ccb223b
JeongMinCha <cjm9236@naver.com>
2016-06-15 18:02:23 +0900

    ServerConfig: contains the constants about server

---
+++ app/src/main/java/com/swmaestro/roundup/server_connector/ServerConfig.java
@@ -0,0 +1,9 @@
+package com.swmaestro.roundup.server_connector;
+
+/**
+ * Created by JeongMinCha on 16. 6. 15..
+ */
+public class ServerConfig {
+    public static final String BASE_URL = "http://133.130.97.125:8000/api_v1/";
+    public static final String BASE_HEADER = "Content-type application/json";
+}
