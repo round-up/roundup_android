1ed9fa1f6e5a3762352c41082aa1391f076463f6
JeongMinCha <cjm9236@naver.com>
2016-06-16 13:24:53 +0900

    Check input components when sign-up

--- app/src/main/java/com/swmaestro/roundup/login/SignupActivity.java
+++ app/src/main/java/com/swmaestro/roundup/login/SignupActivity.java
@@ -7,15 +7,21 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.swmaestro.roundup.R;
 import com.swmaestro.roundup.home.HomeFeedActivity;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -26,6 +32,10 @@ public class SignupActivity extends AppCompatActivity {
     @InjectView(R.id.input_name) EditText _nameText;
     @InjectView(R.id.input_email) EditText _emailText;
     @InjectView(R.id.input_password) EditText _passwordText;
+    @InjectView(R.id.input_password_check) EditText _passwordCheckText;
+    @InjectView(R.id.input_birth) EditText _birthText;
+    @InjectView(R.id.input_phone_number) EditText _phoneNumberText;
+    @InjectView(R.id.input_gender) ToggleButton _genderButton;
     @InjectView(R.id.btn_signup) Button _signupButton;
     @InjectView(R.id.link_login) TextView _loginLink;
 
@@ -105,6 +115,10 @@ public class SignupActivity extends AppCompatActivity {
         String name = _nameText.getText().toString();
         String email = _emailText.getText().toString();
         String password = _passwordText.getText().toString();
+        String passwordCheck = _passwordCheckText.getText().toString();
+        String birthDate = _birthText.getText().toString();
+        String phoneNumber = _phoneNumberText.getText().toString();
+        Boolean gender = _genderButton.isChecked();
 
         if (name.isEmpty() || name.length() < 3) {
             _nameText.setError("at least 3 characters");
@@ -125,6 +139,28 @@ public class SignupActivity extends AppCompatActivity {
             valid = false;
         } else {
             _passwordText.setError(null);
+        }
+
+        if (passwordCheck.isEmpty() || !passwordCheck.equals(password)) {
+            _passwordCheckText.setError("Check password is different.");
+            valid = false;
+        } else {
+            _passwordCheckText.setError(null);
+        }
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date date = sdf.parse(birthDate);
+            if (!birthDate.equals(sdf.format(date))) {
+                _birthText.setError("Birth date format is wrong. You should input it like yyyy-MM-dd");
+                valid = false;
+            } else {
+                Log.i("wow!", "Match Success!");
+            }
+        } catch (ParseException e) {
+            _birthText.setError("Parse Exception");
+            e.printStackTrace();
+            valid = false;
         }
 
         return valid;
