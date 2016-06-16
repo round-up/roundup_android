a507b697fccffafad44eabb551f4494dfde518f5
JeongMinCha <cjm9236@naver.com>
2016-06-16 11:03:36 +0900

    SplashActivity with simple layout

---
+++ app/src/main/java/com/swmaestro/roundup/login/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.swmaestro.roundup.login;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.swmaestro.roundup.R;
+import com.swmaestro.roundup.home.HomeFeedActivity;
+
+/**
+ * Created by JeongMinCha on 16. 6. 16..
+ */
+public class SplashActivity extends Activity {
+    int SPLASH_TIME=2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                overridePendingTransition(0,android.R.anim.fade_in);
+                finish();
+            }
+        },SPLASH_TIME);
+    }
+}
