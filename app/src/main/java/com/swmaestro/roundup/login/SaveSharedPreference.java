814884041d7f504baafbcec7a40ea21d7d479362
JeongMinCha <cjm9236@naver.com>
2016-06-16 10:53:37 +0900

    SaveSharedPrefreneces to implement auto login.

---
+++ app/src/main/java/com/swmaestro/roundup/login/SaveSharedPreference.java
@@ -0,0 +1,26 @@
+package com.swmaestro.roundup.login;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by JeongMinCha on 16. 6. 16..
+ */
+public class SaveSharedPreference {
+    static final String PREF_USER_NAME = "username";
+
+    static SharedPreferences getSharedPreferences(Context ctxt) {
+        return PreferenceManager.getDefaultSharedPreferences(ctxt);
+    }
+
+    public static void setUserName(Context ctxt, String userName) {
+        SharedPreferences.Editor editor = getSharedPreferences(ctxt).edit();
+        editor.putString(PREF_USER_NAME, userName);
+        editor.commit();
+    }
+
+    public static String getUserName(Context ctxt) {
+        return getSharedPreferences(ctxt).getString(PREF_USER_NAME, "");
+    }
+}
